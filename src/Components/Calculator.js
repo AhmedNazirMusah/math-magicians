1d6ed376ca1d1fb58b066cb9c363df526bce4e4e
ahmednazirmusah <ahmed999nazir@gmail.com>
2022-10-27 08:18:16 +0000

    added math logic

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
     this.allbuttons = [
-      'AC', '+/-', '%', '/',
-      '7', '8', '9', '*',
+      'AC', '+/-', '%', '÷',
+      '7', '8', '9', 'x',
       '6', '5', '4', '-',
       '3', '2', '1', '+',
       '0', '.', '=',
     ];
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
+    const {
+      target: { textContent: buttonName },
+    } = e;
+    const newState = calculate(this.state, buttonName);
+    this.setState(newState);
   }
 
   render() {
+    const { total, next } = this.state;
     return (
       <div className="main-div">
-        <div className="display-div">0</div>
+        <div className="display-div">{next || total || 0 }</div>
         <section className="button-container">
           {this.allbuttons.map((btn) => (
-            <button key={btn} type="button" className="buttons">
+            <button
+              key={btn}
+              name={btn}
+              type="button"
+              className="buttons"
+              onClick={(e) => this.handleClick(e)}
+            >
               {btn}
             </button>
           ))}
